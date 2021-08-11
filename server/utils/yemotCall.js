10a21aa2688746809f1c1104cf124e953a48dce4
buzz-code <72219643+buzz-code@users.noreply.github.com>
2021-08-11 21:01:56 +0300

    update code for orphaned messages

--- server/utils/yemotCall.js
+++ server/utils/yemotCall.js
@@ -17,6 +17,7 @@ export class YemotCall extends CallBase {
         howManyTeachedByStudentLessonWereToday: 'בכמה שיעורים מסרו תלמידות?',
         howManyMethodicLessonWereToday: 'כמה שיעורים מתודיקה או דיון היו היום?',
         whatTypeOfActivityWasToday: 'איזה סוג פעילות הייתה היום בבית הספר? לצפיה הקישי 1 למסירה הקישי 2',
+        teacherHasNotStudents: 'אין לך תלמידות מקושרות, אנא פני למזכירה',
         whatTypeOfStudentAttendance: 'תלמידה {0}, שיעור מספר {1}, מה היה? צפיה או פרטני הקישי 1, מסירה או מעורבות הקישי 2, דיון הקישי 3, התלמידה חסרה מסיבות אישיות הקישי 4, לתלמידה הבאה הקישי 5',
         dataWasNotSaved: 'ארעה שגיאה, נסי שוב במועד מאוחר יותר',
         dataWasSavedSuccessfully: 'התיקוף הסתיים בהצלחה',
@@ -33,29 +34,25 @@ export class YemotCall extends CallBase {
                 );
             }
 
-            // const existing_report = await queryHelper.getReportByTeacherIdAndToday(this.user.id, teacher.id);
-            // if (existing_report) {
-            //     await this.send(
-            //         this.id_list_message({ type: 'text', text: this.texts.existingReportWillBeDeleted })
-            //     );
-            // }
+            const messages = [format(this.texts.welcomeForTeacher, teacher.name)];
 
-            await this.send(
-                this.id_list_message({ type: 'text', text: format(this.texts.welcomeForTeacher, teacher.name) })
-            );
+            const existing_report = await queryHelper.getReportByTeacherIdAndToday(this.user.id, teacher.id);
+            if (existing_report) {
+                messages.push(this.texts.existingReportWillBeDeleted);
+            }
 
             switch (teacher.teacher_type_id) {
                 case 1:
-                    await this.getSeminarKitaReport(teacher);
+                    await this.getSeminarKitaReport(teacher, messages);
                     break;
                 case 2:
-                    await this.getTrainingReport(teacher);
+                    await this.getTrainingReport(teacher, messages);
                     break;
                 case 3:
-                    await this.getManhaReport(teacher);
+                    await this.getManhaReport(teacher, messages);
                     break;
                 case 4:
-                    await this.getReponsibleReport(teacher);
+                    await this.getReponsibleReport(teacher, messages);
                     break;
                 default:
                     await this.send(
@@ -70,7 +67,7 @@ export class YemotCall extends CallBase {
             await this.send(
                 this.hangup()
             );
-            
+
             // try {
             //     const attReport = {
             //         user_id: this.user.id,
@@ -101,14 +98,36 @@ export class YemotCall extends CallBase {
         }
     }
 
-    async getSeminarKitaReport(teacher) {
-        await this.askForStudentAttendance(teacher.student1);
-        await this.askForStudentAttendance(teacher.student2);
-        await this.askForStudentAttendance(teacher.student3);
+    async getSeminarKitaReport(teacher, messages) {
+        const students = [];
+        if (teacher.student1) {
+            students.push({ num: '1', student: teacher.student1 });
+        }
+        if (teacher.student2) {
+            students.push({ num: '2', student: teacher.student2 });
+        }
+        if (teacher.student3) {
+            students.push({ num: '3', student: teacher.student3 });
+        }
+
+
+        if (!students.length) {
+            await this.send(
+                messages.map(text => this.id_list_message({ type: 'text', text })),
+                this.id_list_message({ type: 'text', text: this.texts.teacherHasNotStudents }),
+                this.hangup()
+            );
+        }
+
+        this.params.studentsAtt = [];
+        for (const student of students) {
+            await this.askForStudentAttendance(student, messages);
+        }
     }
 
-    async getTrainingReport(teacher) {
+    async getTrainingReport(teacher, messages) {
         await this.send(
+            messages.map(text => this.id_list_message({ type: 'text', text })),
             this.read({ type: 'text', text: this.texts.howManyWatchedLessonWereToday },
                 'howManyWatcheds', 'tap', { max: 1, min: 1, block_asterisk: true })
         );
@@ -118,39 +137,38 @@ export class YemotCall extends CallBase {
         );
     }
 
-    async getManhaReport(teacher) {
+    async getManhaReport(teacher, messages) {
         await this.send(
+            messages.map(text => this.id_list_message({ type: 'text', text })),
             this.read({ type: 'text', text: this.texts.howManyMethodicLessonWereToday },
                 'howManyMethodic', 'tap', { max: 1, min: 1, block_asterisk: true })
         );
     }
 
-    async getReponsibleReport(teacher) {
+    async getReponsibleReport(teacher, messages) {
         await this.send(
+            messages.map(text => this.id_list_message({ type: 'text', text })),
             this.read({ type: 'text', text: this.texts.whatTypeOfActivityWasToday },
                 'activityType', 'tap', { max: 1, min: 1, block_asterisk: true })
         );
     }
 
-    async askForStudentAttendance(student) {
-        if (!student) {
-            return;
-        }
+    async askForStudentAttendance({ num, student }, messages) {
         const studentReports = [];
         for (var i = 1; i <= 5; i++) {
             await this.send(
+                messages.map(text => this.id_list_message({ type: 'text', text })),
                 this.read({ type: 'text', text: format(this.texts.whatTypeOfStudentAttendance, student.name, i) },
                     'studentAttendance' + i, 'tap', { max: 1, min: 1, block_asterisk: true })
             );
+            messages.length = 0;
 
-            if (this.params['studentAttendance' + i] == 5) {
+            if (this.params.studentAttendance == 5) {
                 break;
             }
 
-            studentReports.push({
-                ['studentAttendance' + i]: this.params['studentAttendance' + i],
-            })
+            studentReports.push(this.params.studentAttendance);
         }
-        this.params[student.id] = studentReports;
+        this.params.studentsAtt[num] = studentReports;
     }
 }
