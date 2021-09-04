34772b4f500b67973025c9232d0bc521c318e97e
buzz-code <72219643+buzz-code@users.noreply.github.com>
2021-09-04 21:45:10 +0300

    update teacher excel import

--- client/containers/excel-import/ExcelImportContainer.js
+++ client/containers/excel-import/ExcelImportContainer.js
@@ -6,11 +6,26 @@ import ExcelImport from '../../../common-modules/client/components/excel-import/
 
 const title = 'העלאת קובץ';
 const supportedEntities = [
-  { value: entities.TEACHERS, title: titles.TEACHERS, columns: ['tz', 'name', 'phone'] },
+  {
+    value: entities.TEACHERS,
+    title: titles.TEACHERS,
+    columns: [
+      'tz',
+      'name',
+      'phone',
+      'school',
+      'teacher_type_id',
+      'price',
+      'department',
+      'student_tz_1',
+      'student_tz_2',
+      'student_tz_3',
+    ],
+  },
   { value: entities.STUDENTS, title: titles.STUDENTS, columns: ['tz', 'name'] },
-  { value: entities.ATT_TYPES, title: titles.ATT_TYPES, columns: ['key', 'name'] },
-  { value: entities.TEACHER_TYPES, title: titles.TEACHER_TYPES, columns: ['key', 'name'] },
-  { value: entities.PRICES, title: titles.PRICES, columns: ['key', 'name'] },
+  // { value: entities.ATT_TYPES, title: titles.ATT_TYPES, columns: ['key', 'name'] },
+  // { value: entities.TEACHER_TYPES, title: titles.TEACHER_TYPES, columns: ['key', 'name'] },
+  // { value: entities.PRICES, title: titles.PRICES, columns: ['key', 'name'] },
 ];
 
 const ExcelImportContainer = () => {
