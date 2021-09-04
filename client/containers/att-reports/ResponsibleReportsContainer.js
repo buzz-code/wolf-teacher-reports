928d2c6418163656ecf7aa43b3ca5b0bc480d448
buzz-code <72219643+buzz-code@users.noreply.github.com>
2021-09-04 22:20:02 +0300

    add column order to teacher name fields

--- client/containers/att-reports/ResponsibleReportsContainer.js
+++ client/containers/att-reports/ResponsibleReportsContainer.js
@@ -3,7 +3,7 @@ import React, { useMemo } from 'react';
 import Table from '../../../common-modules/client/components/table/Table';
 
 const getColumns = () => [
-  { field: 'teacher_name', title: 'שם המורה' },
+  { field: 'teacher_name', title: 'שם המורה', columnOrder: 'teachers.name' },
   { field: 'report_date', title: 'תאריך הדיווח', type: 'date' },
   { field: 'activity_type', title: 'סוג פעילות' },
 ];
--- client/containers/att-reports/TrainingReportsContainer.js
+++ client/containers/att-reports/TrainingReportsContainer.js
@@ -3,7 +3,7 @@ import React, { useMemo } from 'react';
 import Table from '../../../common-modules/client/components/table/Table';
 
 const getColumns = () => [
-  { field: 'teacher_name', title: 'שם המורה' },
+  { field: 'teacher_name', title: 'שם המורה', columnOrder: 'teachers.name' },
   { field: 'report_date', title: 'תאריך הדיווח', type: 'date' },
   { field: 'how_many_watched', title: 'כמה שיעורים צפו אצלך?' },
   { field: 'how_many_student_teached', title: 'כמה שיעורים מסרו אצלך?' },
