974e805b777bed4b074d4be504c4d259a2147b0b
buzz-code <72219643+buzz-code@users.noreply.github.com>
2022-11-08 17:52:54 +0200

    fix typo

--- server/routes/working-date.route.js
+++ server/routes/working-date.route.js
@@ -1,6 +1,6 @@
 import { genericRouteWithController } from '../../common-modules/server/controllers/loader';
 
-const router = genericRouteWithController('working-date', 'WorkingDates', (router, ctrl) => {
+const router = genericRouteWithController('working-date', 'WorkingDate', (router, ctrl) => {
     router.route('/get-edit-data')
         .get((req, res) => {
             ctrl.getEditData(req, res);
