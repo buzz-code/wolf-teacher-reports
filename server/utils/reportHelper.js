cec980c9f94cef55eb2a04c8098c86aa8ecdc844
buzz-code <72219643+buzz-code@users.noreply.github.com>
2023-09-19 20:58:23 +0300

    add function joinMultiplePrices

--- server/utils/reportHelper.js
+++ server/utils/reportHelper.js
@@ -9,6 +9,10 @@ function getAnswersPrice() {
     return getCoalesceAndPrice('answers_price.answers_price', 1);
 }
 
+function joinMultiplePrices(...args) {
+    return '(' + args.join(' + ') + ')';
+}
+
 export function getTotalPayForAllTeachers(prices) {
     function getCaseOneLike(number) {
         return `WHEN teachers.teacher_type_id = ${number} THEN ${getTotalPay(number, prices)}`
@@ -49,28 +53,28 @@ export function getTotalPay(teacher_type_id, prices) {
 
 
 export function getTrainingTeacherSalary() {
-    return [
+    return joinMultiplePrices(
         getCoalesceAndPrice('how_many_watched', trainingPrices.watch),
         getCoalesceAndPrice('how_many_student_teached', trainingPrices.teach),
         getCoalesceAndPrice('was_discussing', trainingPrices.discuss),
         getCoalesceAndPrice('how_many_private_lessons', trainingPrices.privateLesson),
         getAnswersPrice(),
-    ].join(' + ')
+    );
 }
 
 export function getSeminarKitaTotalPay(prices) {
-    return [
+    return joinMultiplePrices(
         getCoalesceAndPrice('how_many_watch_or_individual', prices[11]),
         getCoalesceAndPrice('how_many_teached_or_interfering', prices[12]),
         getCoalesceAndPrice('how_many_discussing_lessons', prices[13]),
         getCoalesceAndPrice('was_kamal', prices[14]),
         getCoalesceAndPrice('how_many_lessons_absence', prices[15]),
         getAnswersPrice(),
-    ].join(' + ')
+    );
 }
 
 export function getManhaTotalPay(prices) {
-    return [
+    return joinMultiplePrices(
         getCoalesceAndPrice('how_many_watched_lessons', prices[51]),
         getCoalesceAndPrice('how_many_students_teached', prices[52]),
         getCoalesceAndPrice('how_many_yalkut_lessons', prices[53]),
@@ -78,31 +82,31 @@ export function getManhaTotalPay(prices) {
         getCoalesceAndPrice('how_many_students_help_teached', prices[55]),
         getCoalesceAndPrice('how_many_methodic', prices[56]),
         getAnswersPrice(),
-    ].join(' + ')
+    );
 }
 
 export function getPdsTotalPay(prices) {
-    return [
+    return joinMultiplePrices(
         getCoalesceAndPrice('how_many_watch_or_individual', prices[40]),
         getCoalesceAndPrice('how_many_teached_or_interfering', prices[42]),
         getCoalesceAndPrice('how_many_discussing_lessons', prices[41]),
         getAnswersPrice(),
-    ].join(' + ')
+    );
 }
 
 export function getSpecialEducationTotalPay(prices) {
-    return [
+    return joinMultiplePrices(
         getCoalesceAndPrice('how_many_lessons', 1) + '*' + getCoalesceAndPrice('how_many_students_watched', 1) + '*' + (prices[26] ?? 0),
         getCoalesceAndPrice('how_many_students_teached', prices[27]),
         getCoalesceAndPrice('was_phone_discussing', prices[28]),
         getAnswersPrice(),
-    ].join(' + ')
+    );
 }
 
 export function getKindergartenTotalPay(prices) {
-    return [
+    return joinMultiplePrices(
         getCoalesceAndPrice('how_many_students', prices[24]),
         getCoalesceAndPrice('was_discussing', prices[25]),
         getAnswersPrice(),
-    ].join(' + ')
+    );
 }
