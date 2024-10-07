9d675326c3eb1dd691bab0d89ffbb9cc8c822504
Hadasa Schechter <72219643+buzz-code@users.noreply.github.com>
2024-10-07 19:40:10 +0300

    add 5785 year

--- server/utils/listHelper.js
+++ server/utils/listHelper.js
@@ -2,6 +2,7 @@ export const yearsList = [
     { id: 5782, name: 'תשפ"ב' },
     { id: 5783, name: 'תשפ"ג' },
     { id: 5784, name: 'תשפ"ד' },
+    { id: 5785, name: 'תשפ"ה' },
 ]
 
 export const defaultYear = yearsList[yearsList.length - 1].id;
