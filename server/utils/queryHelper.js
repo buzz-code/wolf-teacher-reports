6636b1a8730adc7b1c2810b8b2036d7d661f6ba4
buzz-code <hadasa.schechter@gmail.com>
2024-12-16 21:28:34 +0200

    refactor updateSalaryMonthByUserId to allow modifing records that are linked to salary month

--- server/utils/queryHelper.js
+++ server/utils/queryHelper.js
@@ -43,7 +43,7 @@ export function saveReportAsConfirmed(id) {
 
 export function updateSalaryMonthByUserId(user_id, ids, salary_month) {
     return new AttReport().query()
-        .where({ user_id, salary_month: null })
+        .where({ user_id })
         .whereIn('id', ids)
         .update({ salary_month });
 }
