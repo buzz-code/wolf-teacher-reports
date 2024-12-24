7de34d999b593239dce2e89309fa30d3911a4398
buzz-code <hadasa.schechter@gmail.com>
2024-12-24 22:19:50 +0200

    change column name

--- client/containers/att-reports/ManhaReportsContainer.js
+++ client/containers/att-reports/ManhaReportsContainer.js
@@ -32,7 +32,7 @@ const getColumns = (handleEditComment) => [
   { field: 'is_taarif_hulia', title: 'חוליה רגילה' },
   { field: 'is_taarif_hulia2', title: 'חוליה גדולה' },
   { field: 'is_taarif_hulia3', title: 'חוליה שניה' },
-  { field: 'how_many_watched_lessons', title: 'שיעורי צפיה או מטלה' },
+  { field: 'how_many_watched_lessons', title: 'מס\' מסירות סבב ב' },
   { field: 'how_many_students_teached', title: 'שיעורי מסירה' },
   { field: 'how_many_yalkut_lessons', title: 'שיעור ילקוט' },
   { field: 'how_many_students_help_teached', title: 'עזרה במרתון או צפיית כיתה שלמה' },
